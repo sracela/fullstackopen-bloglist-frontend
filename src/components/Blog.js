b8f2ab668da0084b65a929ce5f64672f1e02884b
sracela <saracelalfonso@gmail.com>
2020-12-10 22:33:24 +0100

    5.7* Blog list frontend, step7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,39 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import React, {useState} from 'react'
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+
+      <div style={hideWhenVisible}>
+      
+      {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={showWhenVisible}>
+        <div>{blog.title} {blog.author} 
+        <button onClick={toggleVisibility}>hide</button></div> 
+        <p>{blog.url}</p>
+        <p>likes: {blog.likes}</p>
+        {blog.user && 
+        <p>{blog.user.username}</p>}
+        <button onClick={() => console.log("like")}>like</button>
+      </div>
   </div>
-)
+)}
 
 export default Blog
